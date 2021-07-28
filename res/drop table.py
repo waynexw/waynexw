728ccdcccc2bcf08bf2204eab59e120244d939cd
Wayne <72367965+waynexw@users.noreply.github.com>
2021-07-28 21:41:14 +1000

    Create drop table.py

---
+++ res/drop table.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "DROP TABLE IF EXISTS customers"
+
+mycursor.execute(sql)
