6f733e7d4b9216bde43a76d4087aa8c951f9bf56
Wayne <72367965+waynexw@users.noreply.github.com>
2021-07-31 09:49:10 +1000

    Update drop table.py

--- res/drop table.py
+++ res/drop table.py
@@ -32,3 +32,13 @@ mycursor.execute(sql, val)
 mydb.commit()
 
 print(mycursor.rowcount, "record(s) affected")
+
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+for x in mycol.find({},{ "_id": 0, "name": 1, "address": 1 }):
+  print(x)
