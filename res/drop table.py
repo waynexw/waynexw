3067c1963dbf67c3313b28226c7f8895a6fa5281
Wayne <72367965+waynexw@users.noreply.github.com>
2021-07-30 10:58:23 +1000

    Update drop table.py

--- res/drop table.py
+++ res/drop table.py
@@ -12,3 +12,23 @@ mycursor = mydb.cursor()
 sql = "DROP TABLE IF EXISTS customers"
 
 mycursor.execute(sql)
+
+# update data
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "UPDATE customers SET address = %s WHERE address = %s"
+val = ("Valley 345", "Canyon 123")
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) affected")
