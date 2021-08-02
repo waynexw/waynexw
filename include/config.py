8aa997ebd898dacb30f1bd5d4e048e430ec71f4a
Wayne <72367965+waynexw@users.noreply.github.com>
2021-08-02 11:19:49 +1000

    Update config.py

--- include/config.py
+++ include/config.py
@@ -2,3 +2,30 @@
 from __future__ import unicode_literals
 
 AUTO_REPLY_CONTENT = """
+
+# # get the data from local json file. -wayne W
+# # with open("./routes/data1.json", 'r', encoding='utf-8') as f:
+# # 打开数据库连接
+# db = pymysql.connect(**config)
+# # 使用cursor()方法获取操作游标
+# cursor = db.cursor()
+# # SQL 查询语句
+# sql = "SELECT * FROM book_info;"
+# try:
+#    # 执行SQL语句
+#    cursor.execute(sql)
+#    # 获取所有记录列表
+#    results = cursor.fetchall()
+
+# # json_data = json.load(f)
+# # print(json_data)
+# # BOOK_REQUESTS = json_data
+#    BOOK_REQUESTS = results
+# except:
+#    print ("Error: unable to fetch the data")
+ 
+# # 关闭数据库连接
+# finally:
+# #cursor.close()
+# #conn.close()
+#    db.close()
