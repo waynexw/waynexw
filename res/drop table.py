bc06e3262e043dd8ce9e914ed4de116f3c9cefa4
Wayne <72367965+waynexw@users.noreply.github.com>
2021-08-07 16:52:19 +1000

    Update drop table.py

--- res/drop table.py
+++ res/drop table.py
@@ -70,3 +70,22 @@ myresult = mycursor.fetchall()
 
 for x in myresult:
   print(x)
+
+# Start from position 3, and return 5 records:
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM customers LIMIT 5 OFFSET 2")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
