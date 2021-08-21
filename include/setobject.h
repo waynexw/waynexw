6cd3947224068709bf83d43591851b5798bbb53b
Wayne <72367965+waynexw@users.noreply.github.com>
2021-08-21 12:39:33 +1000

    Update setobject.h

--- include/setobject.h
+++ include/setobject.h
@@ -1,7 +1,32 @@
-typedef struct {
-    PyObject *key;
-    Py_hash_t hash;             /* Cached hash code of the key */
-} setentry;
+// typedef struct {
+//     PyObject *key;
+//     Py_hash_t hash;             /* Cached hash code of the key */
+// } setentry;
+
+# Join users and products to see the name of the users favorite product:
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT \
+  users.name AS user, \
+  products.name AS favorite \
+  FROM users \
+  INNER JOIN products ON users.fav = products.id"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
 
 
 import mysql.connector
