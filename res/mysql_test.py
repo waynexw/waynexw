cb77b91c2eeaf4dba3d9a851af91d4839e86a647
Wayne <72367965+waynexw@users.noreply.github.com>
2021-08-23 09:58:26 +1000

    Update mysql_test.py

--- res/mysql_test.py
+++ res/mysql_test.py
@@ -115,3 +115,28 @@ myresult = mycursor.fetchall()
 
 for x in myresult:
   print(x)
+
+# Join users and products to see the name of the users favorite product:
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT \
+  users.name AS user, \
+  products.name AS favorite \
+  FROM users \
+  INNER JOIN products ON users.fav = products.id"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
