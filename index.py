d3e6b5e3a2b1790ffab9f29dcb95bfdcc813ca27
Wayne <72367965+waynexw@users.noreply.github.com>
2021-09-09 19:04:47 +1000

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,25 @@
 https://waynexw.github.io/
+  
+import os
+import uuid
+import json  
+import pymysql  # need to install pymysql first -wayne W
+import mysql.connector 
+from datetime import datetime, timedelta
+from flask import jsonify, abort, request, Blueprint
+from validate_email import validate_email
+
+REQUEST_API = Blueprint('request_api', __name__)
+
+def get_blueprint():
+    """Return the blueprint for the main app module"""
+    return REQUEST_API
+
+# Decide to use json or MySQL database - wayneW.
+with open("./routes/env_conf.json", 'r', encoding='utf-8') as ec:
+    json_data = json.load(ec)
+  # print(json_data)
+# DB_TYPE = json_data['DB_TYPE']
+# DB_NAME = json_data['DB_NAME']
+
+DB_TYPE = str(os.getenv('dbtype')) # use sys env viriables to set DB_TYPE
