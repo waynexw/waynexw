c67ad9822691f3fe9e71cd7bdb87e642ce36305d
Wayne <72367965+waynexw@users.noreply.github.com>
2022-04-17 07:58:28 +1000

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,4 +1,4 @@
-# info of mysql connection. --Pip3 install mysql_connector_python
+# the info of mysql connection. --Pip3 install mysql_connector_python
 
 import mysql.connector
 mydb=mysql.connector.connect(host="localhost",user="root",passwd="password123") 
