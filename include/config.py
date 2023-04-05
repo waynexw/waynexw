b9a168a0b6a92a99a7c0e3018b2e46702c24cbf5
Wayne <72367965+waynexw@users.noreply.github.com>
2023-04-05 11:35:35 +1000

    Update config.py

--- include/config.py
+++ include/config.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 AUTO_REPLY_CONTENT = """
 
-# # obtain config data from local json files. -wayneW
+# # obtain the config data from local json files. -wayneW
 # # with open("./routes/data1.json", 'r', encoding='utf-8') as f:
 # # 打开数据库连接
 # db = pymysql.connect(**config)
