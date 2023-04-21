05570160847956652e14506953697f75655480b6
Wayne <72367965+waynexw@users.noreply.github.com>
2023-04-21 13:49:03 +1000

    Update index.py

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ def get_blueprint():
     """Return the blueprint for the main app module"""
     return REQUEST_API
 
-# Decide to use json or MySQL database - wayneW.
+# Decide to use json or MySQL databases - wayneW.
 with open("./routes/env_conf.json", 'r', encoding='utf-8') as ec:
     json_data = json.load(ec)
   # print(json_data)
